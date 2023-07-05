160fa94bce6638ce2bf46c8a1fc774473cc68a89
Nick DiMola <nick@sdsolutions.io>
2023-07-04 22:41:21 -0400

    v15.2.9
    Improved loader

--- src/app/modules/loader/components/loader.component.ts
+++ src/app/modules/loader/components/loader.component.ts
@@ -13,4 +13,19 @@ export class LoaderComponent {
     loadingText: 'Loading',
   } as LoaderText;
 
+  @Input()
+  disableText: boolean = false;
+
+  protected backgroundStyle: string | undefined;
+
+  private _colors: Array<string> | undefined;
+
+  @Input()
+  set colors(colors: Array<string>) {
+    if (colors.length === 3) {
+      this._colors = colors;
+      this.backgroundStyle = 'linear-gradient(to right, rgba(0,0,0,0) 0%, ' + colors[0] + ' 33%, ' + colors[1] + ' 50% , ' + colors[2] + ' 66%, rgba(0,0,0,0) 100%)';
+    }
+  }
+
 }
