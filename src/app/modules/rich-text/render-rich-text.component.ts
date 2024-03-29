879dad62a96ca139c3c3d735cd87318015330a31
Nick DiMola <nick@sdsolutions.io>
2024-03-29 16:21:31 -0400

    Rich text renderer RTL

--- src/app/modules/rich-text/render-rich-text.component.ts
+++ src/app/modules/rich-text/render-rich-text.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'particle-render-rich-text',
-  template: `<div [innerHTML]="html | dompurify"></div>`
+  template: `<div class="ent_r2l_txt" [innerHTML]="html | dompurify"></div>`
 })
 export class RenderRichTextComponent {
 
