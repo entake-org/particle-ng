dde32723c8e939c1fa921b8f8e8c90291e5fbbea
Nick DiMola <nick@entake.io>
2026-06-24 15:10:43 -0400

    v21.2.12
    - More SSR fixes

--- src/app/shared/components/scroll-to-top/scroll-to-top.component.ts
+++ src/app/shared/components/scroll-to-top/scroll-to-top.component.ts
@@ -1,16 +1,17 @@
-import { AfterViewInit, Component, OnDestroy, Renderer2, inject, input } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
-import {ScrollToTopText} from '../../models/particle-component-text.model';
-import { AsyncPipe } from '@angular/common';
+import { AfterViewInit, Component, OnDestroy, Renderer2, inject, input, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, AsyncPipe } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { ScrollToTopText } from '../../models/particle-component-text.model';
 
 @Component({
-    selector: 'particle-scroll-to-top',
-    templateUrl: './scroll-to-top.component.html',
-    imports: [AsyncPipe]
+  selector: 'particle-scroll-to-top',
+  templateUrl: './scroll-to-top.component.html',
+  imports: [AsyncPipe]
 })
 export class ScrollToTopComponent implements OnDestroy, AfterViewInit {
-  renderer = inject(Renderer2);
-
+  private renderer = inject(Renderer2);
+  private platformId = inject(PLATFORM_ID);
+  private isBrowser = isPlatformBrowser(this.platformId);
 
   readonly content = input<HTMLElement>(null as any);
 
@@ -20,23 +21,29 @@ export class ScrollToTopComponent implements OnDestroy, AfterViewInit {
 
   readonly text = input<ScrollToTopText>({
     scrollToTop: 'Scroll back to the top of the page'
-} as ScrollToTopText);
+  } as ScrollToTopText);
 
   $scrollTop = new BehaviorSubject<number>(0);
 
   private _listener: any;
 
   ngAfterViewInit(): void {
-    this._listener = this.renderer.listen(this.content(), 'scroll', () => {
-      this.$scrollTop.next(this.content().scrollTop);
-    });
+    if (this.isBrowser && this.content()) {
+      this._listener = this.renderer.listen(this.content(), 'scroll', () => {
+        this.$scrollTop.next(this.content().scrollTop);
+      });
+    }
   }
 
   ngOnDestroy(): void {
-    this._listener();
+    if (this._listener) {
+      this._listener();
+    }
   }
 
   scrollToTop(): void {
+    if (!this.isBrowser) return;
+
     const content = this.content();
     if (content) {
       content.scroll({
@@ -45,5 +52,4 @@ export class ScrollToTopComponent implements OnDestroy, AfterViewInit {
       });
     }
   }
-
 }
